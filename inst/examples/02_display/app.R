0b5177f4437eacee267adc66ae007c2329d7e021
TuCai <11544691+TuCai@users.noreply.github.com>
2019-01-02 17:09:02 -0500

    Updated 02_display/app.R

--- inst/examples/02_display/app.R
+++ inst/examples/02_display/app.R
@@ -103,17 +103,12 @@ server <- function(input, output, session) {
     f3
   })
 
-  # -------------------- Sidebar: Select Script  ----------------------------
+  # -------------------- 0 Sidebar: Select Script  --------------------------
   output$selectUI <- renderUI({ selectInput("file", "Select Script:", sel) })
 
   output$result <- renderText({ paste("Script File ID: ", input$file) })
   output$sel_fn <- renderText({ paste(fns[input$file,"file"]) })
   output$yml_name <- renderText({ as.character(fn()) })
-  output$show_script <- reactive({
-    f1 <- paste(fns[input$file,"file"])
-    grepl('^Draw_Dist', f1, ignore.case = TRUE)
-  })
-  outputOptions(output, 'show_script', suspendWhenHidden = FALSE)
   output$show_script_ui <- reactive({
     # Show Shiny inputs if available, react if input file changes therefore access it
     testForShinyUI <- FALSE
@@ -126,141 +121,6 @@ server <- function(input, output, session) {
     testForShinyUI
   })
   outputOptions(output, 'show_script_ui', suspendWhenHidden = FALSE)
-
-  output$show_search <- reactive({ ifelse(input$src =="search",TRUE, FALSE) })
-  outputOptions(output, 'show_search', suspendWhenHidden = FALSE)
-  output$search_for <- renderUI({
-    if (input$src =="search") {
-    tagList(
-    textInput("filename", label="File name:", value = "*.yml"
-              , placeholder = "Pattern for searching file names"),
-    textInput("stext", label="Key word: ", value = ""
-              , placeholder = "key words in files")
-    )
-    }
-  })
-
-
-  # -------------------- tabPanel: Script  ----------------------------------
-  output$script <- renderText({
-    # formulaText()
-    # getURI(d())
-    # getURLContent(d())
-    # convert /x/y/a_b_sas.yml to /x/y/a_b.sas
-    f1 <- gsub('_([[:alnum:]]+).([[:alnum:]]+)$','.\\1',fn())
-    if (!exists("f1")) { return('') }
-    if (is.na(f1) || is.null(f1) || length(f1) < 1 ) {
-      return('ERROR: Missing file name.')
-    }
-    if (!file.exists(f1) && !url.exists(f1)) {
-      return(paste0('ERROR: file ', f1, ' does not exist.'))
-    }
-    ft <- gsub('.+\\.(\\w+)$','\\1', f1)
-    if (length(ft) > 0 && grepl('^(zip|exe|bin)', ft, ignore.case = TRUE) ) {
-      paste(paste0("File: ", f1),"     Could not be displayed.", sep = "\n")
-    } else {
-      paste(paste0("File: ", f1),readChar(f1,nchars=1e6), sep = "\n")
-    }
-  })
-
-  # -------------------- tabPanel: YML  -------------------------------------
-  output$yml <- renderText({
-    f1 <- fn()
-    if (!file.exists(f1) && !url.exists(f1)) {
-      return(paste0('ERROR: file ', f1, ' does not exist.'))
-    }
-    paste(paste0("File: ", f1),readChar(f1,nchars=1e6), sep = "\n")
-  })
-
-  # -------------------- tabPanel: Info  ------------------------------------
-  output$finfo <- renderTable({
-    t1 <- t(fns[input$file,])
-    # t1["file_url",] <- sprintf(h_a, t1["file_url",], t1["file_url",], t1["file",])
-    t1
-  }, rownames = TRUE )
-
-  # -------------------- tabPanel: Metadata  --------------------------------
-  output$mtable <- renderTable({
-    # yaml.load_file(URLencode(f1()[input$file,4]))
-    m2()
-  })
-
-  # -------------------- tabPanel: Verify  ----------------------------------
-  output$verify <- renderTable({ extract_fns(read_yml(fn())) }, rownames = TRUE )
-
-  # -------------------- tabPanel: Download  --------------------------------
-  output$dnload <- renderTable({
-    y1 <- extract_fns(read_yml(fn()))
-    y2 <- download_fns(y1)
-    f1 <- y2[y2$tag=="script_name","file_path"]
-    if (exists("session$clientdata")) { session$clientdata$ofn <- f1 }
-    y2
-  }, rownames = TRUE )
-
-  # -------------------- tabPanel: Merge  -----------------------------------
-  output$merge <- renderTable({
-    f1 <- fn()
-    file_name <- basename(f1)
-    work_dir  <- crt_workdir(to_crt_dir = FALSE)
-    f2 <- paste(work_dir, "scripts", file_name, sep = '/')
-    if (file.exists(f2)) {
-      a  <- read_yml(f1)
-      b  <- read_yml(f2)
-      c  <- merge_lists(a,b)
-      cvt_list2df(c)
-    } else {
-      msg <- c(paste0("Repo: ", f1), paste0("Loc : ", f2, " does not exists."));
-      data.frame(msg)
-    }
-    }, rownames = TRUE )
-
-  # -------------------- tabPanel: Diff  ------------------------------------
-  output$diff <- renderPrint({
-    f1 <- fn()
-    file_name <- basename(f1)
-    work_dir  <- crt_workdir(to_crt_dir = FALSE)
-    f2 <- paste(work_dir, "scripts", file_name, sep = '/')
-    if (file.exists(f2)) {
-      # diffFile(f1,f2, pager = "off")
-      diffFile(f1,f2)
-    } else {
-      msg <- c(paste0("Repo: ", f1), paste0("Loc : ", f2, " does not exists."));
-      msg
-    }
-  })
-
-  # -------------------- tabPanel: Execute  ---------------------------------
-  output$execute <- renderPlot({
-    # if (!is.null(input$yml_name)) {
-    #  y2 <- input$yml_name
-    # } else if (is.null(session$clientdata$ofn)) {
-    #  f1 <- ifelse(input$src=="loc","file_path", "file_url")
-    #  y1 <- download_fns(extract_fns(read_yml(fn())))
-    #  y2 <- as.character(y1[y1$tag=="script_name",f1])
-    # } else {
-    #   y2 <- session$clientdata$ofn
-    # }
-    y2 <- gsub('_([[:alnum:]]+).([[:alnum:]]+)$','.\\1',fn())
-    # str(y2)
-    commandArgs <- function() list(prg="phuse", p1=input$p1, p2=input$p2, script_name=y2)
-    if ( endsWith(toupper(y2),"R")) {
-      source(y2, local = TRUE)
-    } else {
-      # show an error
-      print (append("Not an R script: ",y2))
-      plotMessage("Cannot execute other than an R script.")
-    }
-  })
-
-  # -------------------- tabPanel: Search  ----------------------------------
-  in_fn <- reactive({ input$filename })
-  output$search <- renderDataTable({
-    ff <- search_github(filename=in_fn(), out_type = 'fnlist')
-    cn <- c("fn_id", "script", "file");
-    datatable(ff[,cn]);
-  })
-
-  # -------------------- tabPanel: Script Inputs  ---------------------------
   output$script_inputs <- renderUI({
     # get inputs that are of the type RShinyUI
     y1 <- get_inputs(fn())
@@ -296,31 +156,141 @@ server <- function(input, output, session) {
           eval(parse(text=y1$RShinyUIs[[10]]$control))
         }
       )}
-
   })
 
-  output$script_ins <- renderUI({
-    f1 <- paste(fns[input$file,"file"])
-    if (grepl('^Draw_Dist', f1, ignore.case = TRUE)) {
-      # y1 <- build_inputs(fn())
-      c1 <- c("Normal"="rnorm","Uniform"="runif","Log-normal"="rlnorm","Exponential"="rexp");
-      p1 <- c("p1","Distribution type:", c1);
-      c2 <- c(value = 500,min = 1,max = 1000);
-      p2 <- c("p2","Number of observations:",c2);
-      tagList(
-        radioButtons("p1","Distribution type:", c1)
-      , sliderInput("p2","Number of observations:",value = 500,min = 1,max = 1000)
-      )
-     tagList(
-        sliderInput("p2","Number of observations:",value = 500,min = 1,max = 1000)
-      , radioButtons("p1","Distribution type:",
-        c("Normal"="rnorm","Uniform"="runif","Log-normal"="rlnorm","Exponential"="rexp")
-        )
-     )
-     ## eval(call(y1))
-     ## includeScript("www/s01.R")
+  output$show_search <- reactive({ ifelse(input$src =="search",TRUE, FALSE) })
+  outputOptions(output, 'show_search', suspendWhenHidden = FALSE)
+  output$search_for <- renderUI({
+    if (input$src =="search") {
+    tagList(
+    textInput("filename", label="File name:", value = "*.yml"
+              , placeholder = "Pattern for searching file names"),
+    textInput("stext", label="Key word: ", value = ""
+              , placeholder = "key words in files")
+    )
     }
   })
+
+
+  # -------------------- 1 tabPanel: Script  --------------------------------
+  output$script <- renderText({
+    # formulaText()
+    # getURI(d())
+    # getURLContent(d())
+    # convert /x/y/a_b_sas.yml to /x/y/a_b.sas
+    f1 <- gsub('_([[:alnum:]]+).([[:alnum:]]+)$','.\\1',fn())
+    if (!exists("f1")) { return('') }
+    if (is.na(f1) || is.null(f1) || length(f1) < 1 ) {
+      return('ERROR: Missing file name.')
+    }
+    if (!file.exists(f1) && !url.exists(f1)) {
+      return(paste0('ERROR: file ', f1, ' does not exist.'))
+    }
+    ft <- gsub('.+\\.(\\w+)$','\\1', f1)
+    if (length(ft) > 0 && grepl('^(zip|exe|bin)', ft, ignore.case = TRUE) ) {
+      paste(paste0("File: ", f1),"     Could not be displayed.", sep = "\n")
+    } else {
+      paste(paste0("File: ", f1),readChar(f1,nchars=1e6), sep = "\n")
+    }
+  })
+
+  # -------------------- 2 tabPanel: YML  -----------------------------------
+  output$yml <- renderText({
+    f1 <- fn()
+    if (!file.exists(f1) && !url.exists(f1)) {
+      return(paste0('ERROR: file ', f1, ' does not exist.'))
+    }
+    paste(paste0("File: ", f1),readChar(f1,nchars=1e6), sep = "\n")
+  })
+
+  # -------------------- 3 tabPanel: Info  ----------------------------------
+  output$finfo <- renderTable({
+    t1 <- t(fns[input$file,])
+    # t1["file_url",] <- sprintf(h_a, t1["file_url",], t1["file_url",], t1["file",])
+    t1
+  }, rownames = TRUE )
+
+  # -------------------- 4 tabPanel: Metadata  ------------------------------
+  output$mtable <- renderTable({
+    # yaml.load_file(URLencode(f1()[input$file,4]))
+    m2()
+  })
+
+  # -------------------- 5 tabPanel: Verify  --------------------------------
+  output$verify <- renderTable({ extract_fns(read_yml(fn())) }, rownames = TRUE )
+
+  # -------------------- 6 tabPanel: Download  ------------------------------
+  output$dnload <- renderTable({
+    y1 <- extract_fns(read_yml(fn()))
+    y2 <- download_fns(y1)
+    f1 <- y2[y2$tag=="script_name","file_path"]
+    if (exists("session$clientdata")) { session$clientdata$ofn <- f1 }
+    y2
+  }, rownames = TRUE )
+
+  # -------------------- 7 tabPanel: Merge  ---------------------------------
+  output$merge <- renderTable({
+    f1 <- fn()
+    file_name <- basename(f1)
+    work_dir  <- crt_workdir(to_crt_dir = FALSE)
+    f2 <- paste(work_dir, "scripts", file_name, sep = '/')
+    if (file.exists(f2)) {
+      a  <- read_yml(f1)
+      b  <- read_yml(f2)
+      c  <- merge_lists(a,b)
+      cvt_list2df(c)
+    } else {
+      msg <- c(paste0("Repo: ", f1), paste0("Loc : ", f2, " does not exists."));
+      data.frame(msg)
+    }
+    }, rownames = TRUE )
+
+  # -------------------- 8 tabPanel: Diff  ----------------------------------
+  output$diff <- renderPrint({
+    f1 <- fn()
+    file_name <- basename(f1)
+    work_dir  <- crt_workdir(to_crt_dir = FALSE)
+    f2 <- paste(work_dir, "scripts", file_name, sep = '/')
+    if (file.exists(f2)) {
+      # diffFile(f1,f2, pager = "off")
+      diffFile(f1,f2)
+    } else {
+      msg <- c(paste0("Repo: ", f1), paste0("Loc : ", f2, " does not exists."));
+      msg
+    }
+  })
+
+  # -------------------- 9 tabPanel: Execute  -------------------------------
+  output$execute <- renderPlot({
+    # if (!is.null(input$yml_name)) {
+    #  y2 <- input$yml_name
+    # } else if (is.null(session$clientdata$ofn)) {
+    #  f1 <- ifelse(input$src=="loc","file_path", "file_url")
+    #  y1 <- download_fns(extract_fns(read_yml(fn())))
+    #  y2 <- as.character(y1[y1$tag=="script_name",f1])
+    # } else {
+    #   y2 <- session$clientdata$ofn
+    # }
+    y2 <- gsub('_([[:alnum:]]+).([[:alnum:]]+)$','.\\1',fn())
+    # str(y2)
+    commandArgs <- function() list(prg="phuse", p1=input$p1, p2=input$p2, script_name=y2)
+    if ( endsWith(toupper(y2),"R")) {
+      source(y2, local = TRUE)
+    } else {
+      # show an error
+      print (append("Not an R script: ",y2))
+      plotMessage("Cannot execute other than an R script.")
+    }
+  })
+
+  # -------------------- 10 tabPanel: Search  -------------------------------
+  in_fn <- reactive({ input$filename })
+  output$search <- renderDataTable({
+    ff <- search_github(filename=in_fn(), out_type = 'fnlist')
+    cn <- c("fn_id", "script", "file");
+    datatable(ff[,cn]);
+  })
+
 }
 
 # Create Shiny app ----
